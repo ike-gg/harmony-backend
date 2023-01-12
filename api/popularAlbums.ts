4027d8852918ca52118612b424363431c7a53265
ike <blazej@wodecki.dev>
2023-01-12 13:28:35 +0100

    🔧 CORS update

--- api/popularAlbums.ts
+++ api/popularAlbums.ts
@@ -2,8 +2,6 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import popularAlbumsCall from "../lib/popularAlbumsCall";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", "*");
   try {
     const data = await popularAlbumsCall();
     res.statusCode = 200;
