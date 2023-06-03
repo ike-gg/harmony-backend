a1f7df66fe1d49459ef16618a46428739437c93c
ike <67127723+ike-gg@users.noreply.github.com>
2023-06-03 07:37:51 +0200

    feat: removed ping endpoint to fit in free serverless functions limit

--- api/ping.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  console.log(req.query);
-  res.statusCode = 200;
-  res.send({ ok: "ok" });
-};
